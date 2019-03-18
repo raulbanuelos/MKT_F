f9f53d5c4ec258d54438f3dc1e2bd17b394759be
PixieTaxi <taxi.pixie@gmail.com>
2019-03-18 13:53:58 -0600

    Lectura y carga de archivo consolidado

---
+++ MKT/MKT.Logica/Models/DO_Diario.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MKT.Logica.Models
+{
+    public class DO_Diario
+    {
+        public int ID_DIARIO { get; set; }
+        public string ICC { get; set; }
+        public string DN { get; set; }
+        public string USUARIO { get; set; }
+        public string NOMBRE_CLIENTE { get; set; }
+        public DateTime FECHA_INICIO { get; set; }
+        public string CODIGO_NOMINA_PROMOTOR { get; set; }
+        public string NOMBRE_PROMOTOR { get; set; }
+        public string CODIGO_NOMINA_GERENTE { get; set; }
+        public string ESTATUS { get; set; }
+        public DateTime FECHA_ESTATUS { get; set; }
+        public string OPERADOR_ORIGEN { get; set; }
+        public string OPERADOR_DESTINO { get; set; }
+        public string INTERCONEXION { get; set; }
+        public string NUMERO_FOLIO_ABD { get; set; }
+        public string ESTADO { get; set; }
+        public string APP_ITX { get; set; }
+        public string VALIDACION_INTERCONEXION { get; set; }
+        public DateTime FECHA_RECARGA { get; set; }
+        public string NO_RECARGA { get; set; }
+    }
+}
---
+++ MKT/MKT.Web/Controllers/DiarioController.cs
@@ -0,0 +1,81 @@
+﻿using MKT.DataAccess.ServiceObjects;
+using SpreadsheetLight;
+using DocumentFormat.OpenXml.Spreadsheet;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.IO;
+
+namespace MKT.Web.Controllers
+{
+    public class DiarioController : Controller
+    {
+        // GET: Diario
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Upload()
+        {
+            if (Request.Files.Count > 0)
+            {
+                var file = Request.Files[0];
+                if (file != null && file.ContentLength > 0)
+                {
+                    //string path = "consolidado_" + DateTime.Now.ToLongDateString() + DateTime.Now.ToShortTimeString() + ".xlsx";
+                    var filename = Path.GetFileName(file.FileName);
+                    
+                    var path = Path.Combine(Server.MapPath("~/Files/Consolidado/"), filename);
+                    file.SaveAs(path);
+
+                    SLDocument sL = new SLDocument(path);
+                    int lotes = 5000;
+                    using (var db = new EntitiesMKT())
+                    {
+                        int iRow = 2;
+                        while (!string.IsNullOrEmpty(sL.GetCellValueAsString(iRow, 1)))
+                        {
+                            Diario diario = new Diario();
+                            diario.ICC = sL.GetCellValueAsString(iRow, 1);
+                            diario.DN = sL.GetCellValueAsString(iRow, 2);
+                            diario.USUARIO = sL.GetCellValueAsString(iRow, 3);
+                            diario.NOMBRE_CLIENTE = sL.GetCellValueAsString(iRow, 4);
+                            diario.FECHA_INICIO = sL.GetCellValueAsDateTime(iRow, 5);
+                            diario.CODIGO_NOMINA_PROMOTOR = sL.GetCellValueAsString(iRow, 6);
+                            diario.NOMBRE_PROMOTOR = sL.GetCellValueAsString(iRow, 7);
+                            diario.CODIGO_NOMINA_GERENTE = sL.GetCellValueAsString(iRow, 8);
+                            diario.ESTATUS = sL.GetCellValueAsString(iRow, 10);
+                            diario.FECHA_ESTATUS = sL.GetCellValueAsDateTime(iRow, 11);
+                            diario.OPERADOR_ORIGEN = sL.GetCellValueAsString(iRow, 12);
+                            diario.OPERADOR_DESTINO = sL.GetCellValueAsString(iRow, 13);
+                            diario.INTERCONEXION = sL.GetCellValueAsString(iRow, 14);
+                            diario.NUMERO_FOLIO_ABD = sL.GetCellValueAsString(iRow, 20);
+                            diario.ESTADO = sL.GetCellValueAsString(iRow, 22);
+                            diario.APP_ITX = sL.GetCellValueAsString(iRow, 25);
+                            diario.VALIDACION_INTERCONEXION = sL.GetCellValueAsString(iRow, 26);
+                            diario.FECHA_RECARGA = sL.GetCellValueAsDateTime(iRow, 27);
+                            diario.NO_RECARGA = sL.GetCellValueAsInt32(iRow, 28);
+
+                            db.Diario.Add(diario);
+                            if (iRow % lotes == 0)
+                            {
+                                db.SaveChanges();
+                            }
+
+                            iRow++;
+                        }
+
+                        db.SaveChanges();
+                    }
+                }
+            }
+            
+            return RedirectToAction("Index");
+        }
+        
+    }
+}
